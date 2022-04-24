9c4ad98077adcd566039c80354c3f1607d74905f
KalyDanil <94454437+KalyDanil@users.noreply.github.com>
2022-04-25 00:32:01 +0300

    Delete frontend/src directory

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Auth from './components/Registration/AuthApp';
-
-test('renders learn react link', () => {
-  render(<Auth />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
