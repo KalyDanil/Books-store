827cc4c153e72e490d2c65592bffe4c393e33cb1
KalyDanil <94454437+KalyDanil@users.noreply.github.com>
2022-05-06 12:20:41 +0300

    Delete backend/src directory

--- backend/src/routers/booksRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const booksController = require("../controllers/booksController");
-const booksRouter = express.Router();
-
-booksRouter.get('/', booksController.getAllBooks);
-
-module.exports = booksRouter; 
