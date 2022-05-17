b8596579710f5488f4ed038c41acd32791801fdd
KalyDanil <94454437+KalyDanil@users.noreply.github.com>
2022-05-17 22:25:14 +0300

    Delete frontend/src directory

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Site from './components/Router/Router';
-
-test('renders learn react link', () => {
-  render(<Site />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
