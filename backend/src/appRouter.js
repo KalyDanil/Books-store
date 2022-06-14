0908e05becd601a006b330573c700927d9502786
KalyDanil <94454437+KalyDanil@users.noreply.github.com>
2022-06-14 12:27:31 +0300

    Delete backend/src directory

--- backend/src/appRouter.js
+++
@@ -1,9 +0,0 @@
-const authorizationRouter = require("./routers/authRouter");
-const userRouter = require("./routers/userRouter");
-const booksRouter = require("./routers/booksRouter");
-
-module.exports = appRouter = (app) => {
-    app.use("/main", booksRouter);
-    app.use("/auth", authorizationRouter);
-    app.use("/user", userRouter);
-};
--- backend/src/appUse.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require('cors');
-
-module.exports = appUse = (app) => {
-    app.use(cors({ origin: ['http://localhost:3000'] }));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.static(__dirname + '/public'));
-};
